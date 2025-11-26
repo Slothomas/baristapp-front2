1c2d5960d66322c55b0dcdc390b277ca26dcea88
Baristapp <jose.guzman41@inacapmail.cl>
2025-11-26 17:32:29 -0300

    update https

--- src/api/http.ts
+++ src/api/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getUserMock } from "../api/auth";
 
 const baseURL = import.meta.env.DEV
-  ? "http://127.0.0.1:8000"
+  ? "http://127.0.0.1:8000" // local
   : "https://baristappback-axg6grb2ahaffnby.canadacentral-01.azurewebsites.net";
 
 export const http = axios.create({
@@ -11,7 +11,7 @@ export const http = axios.create({
 });
 
 http.interceptors.request.use((config) => {
-  // Enviar token si existe
+  // Enviar token si existe (si en algún momento lo usas)
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -19,7 +19,7 @@ http.interceptors.request.use((config) => {
 
   const user = getUserMock();
   if (user?.id) {
-    config.headers["X-UserID"] = user.id;         // por seguridad
+    config.headers["X-UserID"] = user.id;
     config.headers["Authorization"] = `UserID ${user.id}`; // requerido por backend
   }
 
